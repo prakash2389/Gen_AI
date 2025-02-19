550a604d30bf2fb0163ea2369b8d0c56cab183e7
Prakash Kumar Thuthika <prakash.thuthika@gmail.com>
2025-02-19 10:15:56 +0530

    Create openai_rag_langchain_pdf.py

---
+++ RAG_langchain/openai_rag_langchain_pdf.py
@@ -0,0 +1,15 @@
+C:\Users\prakash.tutika\.conda\envs\env_langchain1\python.exe D:/RAG_langchain/rag_langchain.py
+2025-02-19 10:17:47.015 WARNING streamlit.runtime.scriptrunner_utils.script_run_context: Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
+2025-02-19 10:17:55.531 
+  Warning: to view this Streamlit app on a browser, run it with the following
+  command:
+
+    streamlit run D:/RAG_langchain/rag_langchain.py [ARGUMENTS]
+2025-02-19 10:17:55.531 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
+2025-02-19 10:18:01.313 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
+2025-02-19 10:18:01.315 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
+2025-02-19 10:18:01.315 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
+2025-02-19 10:18:01.315 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
+2025-02-19 10:18:01.315 Thread 'MainThread': missing ScriptRunContext! This warning can be ignored when running in bare mode.
+
+Process finished with exit code 0
